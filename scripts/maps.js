2cab71290fefdd3b946ea8998f7549d71e53467b
Ayran3 <71779535+Ayran3@users.noreply.github.com>
2021-01-11 15:35:41 -0300

    Add files via upload

---
+++ scripts/maps.js
@@ -0,0 +1,7 @@
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(-14.79096103669138, -39.27627868930688),
+    zoom:20,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
